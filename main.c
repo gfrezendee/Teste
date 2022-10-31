0ee585f2354caeb20b4aea3b38f0b4a82501316a
Gabriel <63487827+biel6959@users.noreply.github.com>
2022-10-31 10:26:02 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -2,7 +2,35 @@
 
 int main() {
 	
-	printf("Hello, World!");
+	printf("\n");
+	printf("------------------- CALCULADORA DE MEDIA -------------------");
+	printf("\n");
+	
+	//-----------------------------------------------------------------------------
+	
+	float P1, P2, T, MF;
+	
+	printf("Informe a nota da primeira prova: ");
+	scanf("%f",&P1);
+	
+	printf("Informe a nota da segunda prova: ");
+	scanf("%f",&P2);
+	
+	printf("Informe a nota de trabalho (caso nao houver, coloque 0): ");
+	scanf("%f",&T);
+	
+	MF=P1+P2+T;
+	
+	printf("\n");
+	printf("Media Final: %.2f", MF);
+	printf("\n");
+	
+	if(MF>=7){
+		printf("APROVADO!");
+	}else{
+		printf("REPROVADO");
+	}
+	
 	
 	return 0;
 }
